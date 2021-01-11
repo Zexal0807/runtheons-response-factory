0b46075aeb48e5cf5182daff438940f1f5853532
Zexal0807 <gallinar00@gmail.com>
2021-01-11 16:11:30 +0100

    Init npm, some fix version 1.0.1

--- ResponseFactory.js
+++ ResponseFactory.js
@@ -1,13 +1,23 @@
 module.exports = class ResponseFactory {
 
-    res = null;
+	res = null;
 
-    setResponse(res) {
-        this.res = res;
-    }
+	setResponse(res) {
+		this.res = res;
+	}
 
-    send(data = {}, option = {}) {
-        this.res.json(data);
-    }
+	send(data, option) {
+		if (res)
+			throw Error('Response not set');
 
+		this.res.set('Content-Type', option.type);
+		this.res.sendStatus(200).send(data);
+	}
+
+	getOption(req) {
+		var header = req.header || {};
+		return {
+			type: header['Content-Type'] ? header['Content-Type'] : 'application/json'
+		};
+	}
 }
