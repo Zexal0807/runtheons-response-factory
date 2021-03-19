564dc8cb4c54245e2d289e5043929f7607a3365c
Zexal0807 <gallinar00@gmail.com>
2021-03-19 14:14:34 +0100

    Add send file

--- ResponseFactory.js
+++ ResponseFactory.js
@@ -1,5 +1,8 @@
 module.exports = new class ResponseFactory {
 
+	FILE = "file";
+	JSON = "application/json";
+
 	res = null;
 
 	setResponse(res) {
@@ -11,15 +14,20 @@ module.exports = new class ResponseFactory {
 			throw Error('Response not set');
 
 		this.res.status(200);
-		this.res.set('Content-Type', option.type);
-		this.res.send(data);
+
+		if (option.type == this.FILE) {
+			this.res.sendFile(data.data);
+		} else {
+			this.res.set('Content-Type', option.type);
+			this.res.send(data);
+		}
 	}
 
 	getOption(req) {
 		var headers = req.headers || {};
 		return {
 			headers: req.headers,
-			type: headers['Content-Type'] ? headers['Content-Type'] : 'application/json'
+			type: headers['Content-Type'] ? headers['Content-Type'] : this.JSON
 		};
 	}
 }
